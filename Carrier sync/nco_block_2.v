54c8e21b81b4c87395ba7b1bfe6ddc9fac4ab7cb
Shivansh joshi <shivanshjoshi5263@gmail.com>
2026-08-04 00:41:59 +0530

    Update and rename nco_block.v to nco_block_2.v

---
+++ Carrier sync/nco_block_2.v
@@ -0,0 +1,62 @@
+module nco_block_2 ( 
+	input signed [11:0] ip_data,
+    input ip_clock,
+    input ip_reset,
+	output reg signed [11:0] op_cosine_wave,
+    output reg signed [11:0] op_sine_wave
+);
+
+	wire [2:0] ip_phase; 
+	assign ip_phase = ip_data[10:8];
+	
+	always@(posedge ip_clock or negedge ip_reset) begin 
+		if(ip_reset == 1'b0) begin 
+			op_sine_wave <= 12'sd0; 
+			op_cosine_wave <= 12'sd0;
+		end else begin  
+			case(ip_phase) 
+				3'b000: begin
+					op_sine_wave   <= 12'sd0;
+					op_cosine_wave <= 12'sd2047;
+				end
+
+				3'b001: begin
+					op_sine_wave   <= 12'sd1447;
+					op_cosine_wave <= 12'sd1447;
+				end
+
+				3'b010: begin
+					op_sine_wave   <= 12'sd2047;
+					op_cosine_wave <= 12'sd0;
+				end
+
+				3'b011: begin
+					op_sine_wave   <= 12'sd1447;
+					op_cosine_wave <= -12'sd1447;
+				end
+
+				3'b100: begin
+					op_sine_wave   <= 12'sd0;
+					op_cosine_wave <= -12'sd2047;
+				end
+
+				3'b101: begin
+					op_sine_wave   <= -12'sd1447;
+					op_cosine_wave <= -12'sd1447;
+				end
+
+				3'b110: begin
+					op_sine_wave   <= -12'sd2047;
+					op_cosine_wave <= 12'sd0;
+				end
+
+				3'b111: begin
+					op_sine_wave   <= -12'sd1447;
+					op_cosine_wave <= 12'sd1447;
+				end
+			endcase 
+		end 
+	end 
+	
+
+endmodule

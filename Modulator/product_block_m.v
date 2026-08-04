02f6f89cd42d67e2d1835596ea65fa52ec3bf709
Shivansh joshi <shivanshjoshi5263@gmail.com>
2026-08-04 18:00:11 +0530

    Update product_block_m.v

--- Modulator/product_block_m.v
+++ Modulator/product_block_m.v
@@ -1,21 +1,22 @@
 
 module product_block_m(
-	input [1:0] ip_data, 
-	input [11:0] ip_data_2,
+	input signed [1:0] ip_data, 
+	input signed [11:0] ip_data_2,
 	input ip_clock, 
 	input ip_reset,
-	output [11:0] op_data
+	output signed [11:0] op_data
 ); 
 
 	
 	reg signed [11:0] reg_product; 
 	wire signed [13:0] wire_product; 
 	assign wire_product = ip_data * ip_data_2;
-	always@(negedge ip_clock or negedge ip_reset) begin 
-		if(ip_reset == 1'd0) begin 
-			reg_product <= 12'd0;
+	
+	always@(posedge ip_clock or negedge ip_reset) begin 
+		if(ip_reset == 1'b0) begin 
+			reg_product <= 12'sd0;
 		end else begin 
-			reg_product <= wire_product[13-:12]; 
+			reg_product <= wire_product[12:1]; 
 		end 
 	end
 

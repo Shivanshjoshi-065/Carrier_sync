fb6d3b6091176d724c3854d892892a94992df429
Shivansh joshi <shivanshjoshi5263@gmail.com>
2026-08-04 17:31:46 +0530

    Update add_block.v

--- Modulator/add_block.v
+++ Modulator/add_block.v
@@ -1,20 +1,22 @@
 
 module add_block(
-	input [11:0] ip_data, 
-	input [11:0] ip_data_2,
+	input signed [11:0] ip_data, 
+	input signed [11:0] ip_data_2,
 	input ip_clock, 
 	input ip_reset,
-	output [11:0] op_data
+	output signed [11:0] op_data
 ); 
-	
+
+
 	reg signed [11:0] reg_add; 
 	wire signed [12:0] wire_add; 
 	assign wire_add = ip_data + ip_data_2;
+	
 	always@(posedge ip_clock or negedge ip_reset) begin 
-		if(ip_reset == 1'b0) begin 
-			reg_add <= 12'd0;
+		if(ip_reset == 1'd0) begin 
+			reg_add <= 12'sd0;
 		end else begin 
-			reg_add <= wire_add[12-:12]; 
+			reg_add <= wire_add[12:1]; 
 		end 
 	end
 

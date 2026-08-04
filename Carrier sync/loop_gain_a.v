e19a1ac5a27b44fcb8f3c0f9e3a0c3556a026b61
Shivansh joshi <shivanshjoshi5263@gmail.com>
2026-08-04 18:07:23 +0530

    Add files via upload

---
+++ Carrier sync/loop_gain_a.v
@@ -0,0 +1,22 @@
+module loop_gain_a( 
+	input signed [11:0] ip_data, 
+	input ip_clock, 
+	input ip_reset, 
+	output signed [11:0] op_data
+); 
+	
+	reg signed [11:0] reg_gain; 
+	wire signed [15:0] wire_gain; 
+	assign wire_gain = ip_data <<< 4; 
+	
+	always@(posedge ip_clock or negedge ip_reset) begin 
+		if(ip_reset == 1'd0) begin 
+			reg_gain <= 12'd0;
+		end else begin 
+			reg_gain <= wire_gain[15:4]; 
+		end 
+	end 
+	
+	assign op_data = reg_gain; 
+
+endmodule 

5769f50f8243aaa4b5462c0e4bc944c13b53e020
Shivansh joshi <shivanshjoshi5263@gmail.com>
2026-08-04 18:01:44 +0530

    Update carrier_synchronizer.v

--- Carrier sync/carrier_synchronizer.v
+++ Carrier sync/carrier_synchronizer.v
@@ -2,20 +2,24 @@ module carrier_synchronizer(
 	input signed [11:0] ip_data, 
 	input ip_clock, 
 	input ip_reset, 
-	output [11:0] op_real, 
-	output [11:0] op_imag
+	output signed [11:0] op_real, 
+	output signed [11:0] op_imag
 ); 
-	
+
+	// ------------------------------------ // 
+	// 			feedback regisers 				 	   
+	// ------------------------------------ // 
 	wire signed [11:0] get_op_cosine_wave; 
-	wire signed [11:0] get_op_sine_wave; 
+	wire signed [11:0] get_op_sine_wave;
 	
-	// ------------------------------------  
+	
+	// ------------------------------------ // 
 	// 			Costas loop				 	   
-	// ------------------------------------  
+	// ------------------------------------ // 
 	wire signed [11:0] get_op_costas_err; 
 	wire signed [11:0] get_op_data_i; 
-	wire signed [11:0] get_op_data_q; 
-	
+	wire signed [11:0] get_op_data_q;  
+	 
 	costas_loop costas_loop_inst( 
 		.ip_unsync(ip_data), 
 		.ip_cosine(get_op_cosine_wave), 
@@ -26,21 +30,24 @@ module carrier_synchronizer(
 		.op_data_i(get_op_data_i), 
 		.op_data_q(get_op_data_q)
 	); 
-	
-	
+
 	// ------------------------------------  
+	// 			Costas feedback 2				 	   
+	// ------------------------------------ 
+	wire signed [11:0] get_op_feedback; 
+	costas_feedback_2 costas_feedback_2_inst( 
+		.ip_data(get_op_costas_err), 
+		.ip_clock(ip_clock), 
+		.ip_reset(ip_reset), 
+		.op_data(get_op_feedback)
+	);   
+	
+	// ------------------------------------ 
 	// 			Nco block				 	   
-	// ------------------------------------  
-	reg [2:0] reg_counter; 
-	always@(negedge ip_clock or negedge ip_reset) begin 
-		if(ip_reset == 1'b0) begin 
-			reg_counter <= 3'b000;
-		end else begin 
-			reg_counter <= reg_counter + 3'b001;
-		end 
-	end  
-	nco_block nco_block_inst( 
-		.ip_phase(reg_counter), 
+	// ------------------------------------ 
+	
+	nco_block_2 nco_block_2_inst( 
+		.ip_data(get_op_feedback), 
 		.ip_clock(ip_clock), 
 		.ip_reset(ip_reset), 
 		.op_cosine_wave(get_op_cosine_wave), 
@@ -49,15 +56,6 @@ module carrier_synchronizer(
 	
 	
 	assign op_real = get_op_data_i; 
-	assign op_imag = get_op_data_q; 
-	
-	
-	always @(negedge ip_clock or negedge ip_reset) begin
-		$display("Carrier: in=%0d op_data_i=%0d op_data_q = %d",
-				 ip_data,
-				 get_op_data_i, 
-				 get_op_data_q);
-	end 
-	
+	assign op_imag = get_op_data_q;
 
 endmodule 

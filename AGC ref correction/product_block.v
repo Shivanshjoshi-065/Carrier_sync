4692f044c8c836e946bb07ec5c8fd83cfb7c3f5a
Shivansh joshi <shivanshjoshi5263@gmail.com>
2026-08-04 17:36:18 +0530

    Update product_block.v

--- AGC ref correction/product_block.v
+++ AGC ref correction/product_block.v
@@ -4,30 +4,22 @@ module product_block(
 	input signed [11:0] ip_data_2,
 	input ip_clock, 
 	input ip_reset,
-	output [11:0] op_data
+	output signed [11:0] op_data
 ); 
 
 	
 	reg signed [11:0] reg_product; 
 	wire signed [23:0] wire_product; 
-	assign wire_product = ip_data * ip_data_2;
-	always@(negedge ip_clock or negedge ip_reset) begin 
-		if(ip_reset == 1'd0) begin 
-			reg_product <= 12'd0;
+	assign wire_product = ip_data * ip_data_2; 
+	
+	always@(posedge ip_clock or negedge ip_reset) begin 
+		if(ip_reset == 1'b0) begin 
+			reg_product <= 12'sd0;
 		end else begin 
-			reg_product <= wire_product[23:12]; 
+			reg_product <= wire_product[22:11]; 
 		end 
 	end
 
 	assign op_data = reg_product;
-	
-	always @(negedge ip_clock or negedge ip_reset) begin
-		$display("PRODUCT: in1=%0d in2=%0d mult=%0d out=%0d",
-				 ip_data,
-				 ip_data_2,
-				 wire_product,
-				 op_data);
-	end 
-	
 	
 endmodule 

c2bb3ce2b7c93f7229d1e3be42a69ba16069e2f5
Shivansh joshi <shivanshjoshi5263@gmail.com>
2026-08-04 17:37:37 +0530

    Update agc_ref_correction_a.v

--- AGC ref correction/agc_ref_correction_a.v
+++ AGC ref correction/agc_ref_correction_a.v
@@ -3,26 +3,34 @@ module agc_ref_correction(
 	input signed [11:0] ip_data, 
 	input ip_clock,
 	input ip_reset, 
-	output [11:0] op_data
+	output signed [11:0] op_data
 ); 
 	
+	// ------------------------------------ // 
+	// 			feedback regisers 				 	   
+	// ------------------------------------ // 
+	wire signed [11:0] get_op_acc;  
+	
+	
 	// ------------------------------------  
-	// 			Gain block 				 	   
+	// 			Product block 				 	   
 	// ------------------------------------  
-	wire signed [11:0] get_op_gain; 
-	gain_block gain_block_inst( 
+	wire signed [11:0] get_op_product; 
+	product_block product_block_inst( 
 		.ip_data(ip_data), 
+		.ip_data_2(get_op_acc),
 		.ip_clock(ip_clock), 
 		.ip_reset(ip_reset), 
-		.op_data(get_op_gain)
-	); 
+		.op_data(get_op_product)
+	);
+	
 	
 	// ------------------------------------  
 	// 			Abs block 				 	   
 	// ------------------------------------  
 	wire signed [11:0] get_op_abs; 
 	abs_block abs_block_inst(
-		.ip_data(get_op_gain), 
+		.ip_data(get_op_product), 
 		.ip_clock(ip_clock), 
 		.ip_reset(ip_reset), 
 		.op_data(get_op_abs)
@@ -40,40 +48,50 @@ module agc_ref_correction(
 	); 
 	
 	// ------------------------------------  
-	// 			Gain calculate  				 	   
+	// 			Gain error  				 	   
 	// ------------------------------------  
-	wire signed [11:0] get_op_gain_error; 
+	wire signed [11:0] get_op_error; 
 	gain_error gain_error_inst( 
 		.ip_data(get_op_power_est), 
 		.ip_clock(ip_clock), 
 		.ip_reset(ip_reset), 
-		.op_data(get_op_gain_error)
+		.op_data(get_op_error)
 	);
+	
+	// ------------------------------------  
+	// 			Loop gain  				 	   
+	// ------------------------------------  
+	wire signed [11:0] get_op_loop_gain; 
+	loop_gain_a loop_gain_a_inst( 
+		.ip_data(get_op_error), 
+		.ip_clock(ip_clock), 
+		.ip_reset(ip_reset), 
+		.op_data(get_op_loop_gain)
+	);  
 	
 	// ------------------------------------  
 	// 			Accumulator 				 	   
 	// ------------------------------------  
-	wire signed [11:0] get_op_acc;  
+	// wire signed [11:0] get_op_acc;  
 	accumulator accumulator_inst(
-		.ip_data(get_op_gain_error), 
+		.ip_data(get_op_loop_gain), 
 		.ip_clock(ip_clock), 
 		.ip_reset(ip_reset), 
 		.op_data(get_op_acc)
 	);
 	
-	// ------------------------------------  
-	// 			Product block 				 	   
-	// ------------------------------------  
-	wire signed [11:0] get_op_product; 
-	product_block product_block_inst( 
-		.ip_data(ip_data), 
-		.ip_data_2(get_op_acc),
-		.ip_clock(ip_clock), 
-		.ip_reset(ip_reset), 
-		.op_data(get_op_product)
-	); 
+	 
+	assign op_data = get_op_product; 
 	
+	always @(posedge ip_clock or negedge ip_reset) begin
+		$display("AGC: in1=%0d abs=%0d power_est=%0d error=%0d acc=%0d product=%0d",
+				 ip_data,
+				 get_op_abs, 
+				 get_op_power_est, 
+				 get_op_error,
+				 get_op_acc, 
+				 get_op_product);
+	end 
 	
-	assign op_data = get_op_product;
 	
 endmodule 

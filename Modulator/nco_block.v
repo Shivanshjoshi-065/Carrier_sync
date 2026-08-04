c495e7717b8d45ba6c81a0cd8ab96ed8746f4079
Shivansh joshi <shivanshjoshi5263@gmail.com>
2026-08-04 17:57:20 +0530

    Update nco_block.v

--- Modulator/nco_block.v
+++ Modulator/nco_block.v
@@ -8,42 +8,49 @@ module nco_block (
 
 	always@(posedge ip_clock or negedge ip_reset) begin 
 		if(ip_reset == 1'b0) begin 
-			op_sine_wave <= 12'b000000000000; 
-			op_cosine_wave <= 12'b000000000000;
+			op_sine_wave <= 12'sd0; 
+			op_cosine_wave <= 12'sd0;
 		end else begin  
 			case(ip_phase) 
-				3'b000: begin 
-					op_sine_wave <= 12'b000000000000; 
-					op_cosine_wave <= 12'b011111111111;
-					end  
-				3'b001: begin 
-					op_sine_wave <= 12'b010110100111; 
-					op_cosine_wave <= 12'b010110100111;
-					end 
-				3'b010: begin 
-					op_sine_wave <= 12'b011111111111; 
-					op_cosine_wave <= 12'b000000000000;
-					end 
-				3'b011: begin 
-					op_sine_wave <= 12'b010110100111; 
-					op_cosine_wave <= 12'b010110100111;
-					end 
-				3'b100: begin 
-					op_sine_wave <= 12'b011111111111; 
-					op_cosine_wave <= 12'b000000000000;
-					end
-				3'b101: begin 
-					op_sine_wave <= 12'b101001011001; 
-					op_cosine_wave <= 12'b101001011001;
-					end
-				3'b110: begin 
-					op_sine_wave <= 12'b100000000001; 
-					op_cosine_wave <= 12'b000000000000;
-					end
-				3'b111: begin 
-					op_sine_wave <= 12'b101001011001; 
-					op_cosine_wave <= 12'b101001011001;
-					end
+				3'b000: begin
+					op_sine_wave   <= 12'sd0;
+					op_cosine_wave <= 12'sd2047;
+				end
+
+				3'b001: begin
+					op_sine_wave   <= 12'sd1447;
+					op_cosine_wave <= 12'sd1447;
+				end
+
+				3'b010: begin
+					op_sine_wave   <= 12'sd2047;
+					op_cosine_wave <= 12'sd0;
+				end
+
+				3'b011: begin
+					op_sine_wave   <= 12'sd1447;
+					op_cosine_wave <= -12'sd1447;
+				end
+
+				3'b100: begin
+					op_sine_wave   <= 12'sd0;
+					op_cosine_wave <= -12'sd2047;
+				end
+
+				3'b101: begin
+					op_sine_wave   <= -12'sd1447;
+					op_cosine_wave <= -12'sd1447;
+				end
+
+				3'b110: begin
+					op_sine_wave   <= -12'sd2047;
+					op_cosine_wave <= 12'sd0;
+				end
+
+				3'b111: begin
+					op_sine_wave   <= -12'sd1447;
+					op_cosine_wave <= 12'sd1447;
+				end
 			endcase 
 		end 
 	end 
